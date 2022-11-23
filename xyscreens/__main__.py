0acafac44caeafdfdbbed07e6dfccf6b0d129589
Rogier van Staveren <rogier@batoid.com>
2022-11-23 23:47:01 +0100

    Introduce argument to wait for the action to finish
    And give feedback about the progress

--- xyscreens/__main__.py
+++ xyscreens/__main__.py
@@ -5,6 +5,7 @@ Created on 18 Nov 2022
 """
 import argparse
 import logging
+import time
 
 from xyscreens.XYScreens import XYScreens
 
@@ -17,6 +18,7 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("port")
     argparser.add_argument("action", choices=["up", "stop", "down"])
+    argparser.add_argument("--wait", dest="wait", action="store_true")
     argparser.add_argument("--debug", dest="debugLogging", action="store_true")
 
     args = argparser.parse_args()
@@ -32,8 +34,36 @@ if __name__ == "__main__":
     screen = XYScreens(args.port)
 
     if args.action == "up":
-        screen.up()
+        screen.set_position(100.0)
+        if screen.up():
+            if args.wait:
+                while True:
+                    state = screen.state()
+                    position = screen.position()
+                    if not args.debugLogging:
+                        print(f"{screen.STATES[state]:8}: {position:5.1f} %", end="\r")
+                    else:
+                        logger.info("%s: %5.1f %%", screen.STATES[state], position)
+                    if state == screen.STATE_UP:
+                        if not args.debugLogging:
+                            print()
+                        break
+                    time.sleep(0.1)
     elif args.action == "stop":
         screen.stop()
     elif args.action == "down":
-        screen.down()
+        screen.set_position(0.0)
+        if screen.down():
+            if args.wait:
+                while True:
+                    state = screen.state()
+                    position = screen.position()
+                    if not args.debugLogging:
+                        print(f"{screen.STATES[state]:8}: {position:5.1f} %", end="\r")
+                    else:
+                        logger.info("%s: %5.1f %%", screen.STATES[state], position)
+                    if state == screen.STATE_DOWN:
+                        if not args.debugLogging:
+                            print()
+                        break
+                    time.sleep(0.1)
