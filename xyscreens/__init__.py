67c2ded802bf0825a4c44b0e4edad4016fa5ee34
Rogier van Staveren <rogier@batoid.com>
2022-11-24 14:16:25 +0100

    Documentation

--- xyscreens/__init__.py
+++ xyscreens/__init__.py
@@ -1,3 +1,11 @@
+"""
+Implements the XYScreens library for controlling XY Screens projector screens
+and projector lifts.
+
+Created on 17 Nov 2022
+
+@author: Rogier van Staveren
+"""
 __version__ = "0.0.2"
 
 from xyscreens.xyscreens import XYScreens
