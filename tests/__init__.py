18a7c7ef93fff81ae6c86ac468bb6f0379b7a426
Rogier van Staveren <rogier@batoid.com>
2024-07-07 13:00:56 +0200

    Use asyncio.run instead of loop.run_until_complete

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,7 +14,7 @@ def async_test(coro):
     def wrapper(*args, **kwargs):
         loop = asyncio.new_event_loop()
         try:
-            return loop.run_until_complete(coro(*args, **kwargs))
+            return asyncio.run(coro(*args, **kwargs))
         finally:
             loop.close()
 
